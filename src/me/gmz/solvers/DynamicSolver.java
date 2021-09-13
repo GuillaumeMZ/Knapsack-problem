ef8f4de18a09bee5da0c720f81efc93351e897f3
GuillaumeMZ <>
2021-09-13 09:11:16 +0200

    Dynamic solver fix
    (it is working with integers only atm)

--- src/me/gmz/solvers/DynamicSolver.java
+++ src/me/gmz/solvers/DynamicSolver.java
@@ -12,28 +12,27 @@ public class DynamicSolver implements ISolver {
     public Backpack solve(ArrayList<Item> data, float backpackWeight) {
         Backpack result = new Backpack(backpackWeight);
 
-        int weight = (int)(backpackWeight*10);
-        float[][] M = new float[data.size()][weight+1];
+        int[][] M = new int[data.size()][(int)backpackWeight+1];
 
-        for(int j = 0; j < weight+1; j++){ //Filling the first line of the array
-            if((int)(data.get(0).getWeight()*10) > j)
+        for(int j = 0; j < backpackWeight+1; j++){ //Filling the first line of the array
+            if((int)(data.get(0).getWeight()) > j)
                 M[0][j] = 0;
             else
-                M[0][j] = data.get(0).getValue();
+                M[0][j] = (int)data.get(0).getValue();
         }
 
         for(int i = 1; i < data.size(); ++i){ //Filling the remaining lines
-            for(int j = 0; j < weight+1; ++j){
-                if((int)(data.get(i).getWeight()*10) > j)
+            for(int j = 0; j < (int)backpackWeight+1; ++j){
+                if((int)(data.get(i).getWeight()) > j)
                     M[i][j] = M[i-1][j];
                 else
-                    M[i][j] = Math.max(M[i-1][j], M[i-1][j-(int)(data.get(i).getWeight()*10)] + data.get(i).getValue());
+                    M[i][j] = Math.max(M[i-1][j], M[i-1][j-(int)(data.get(i).getWeight())] + (int)data.get(i).getValue());
             }
         }
 
         //Now that the matrix is filled, we can find the best solution and deduce the items
         int i = data.size() - 1;
-        int j = weight;
+        int j = (int)backpackWeight;
 
         while(M[i][j] == M[i][j-1])
             j--;
@@ -42,17 +41,17 @@ public class DynamicSolver implements ISolver {
             while(i > 0 && M[i][j] == M[i-1][j]){
                 i--;
             }
-            j = j - (int)(data.get(i).getWeight() * 10);
-            if(j > 0){
+            j = j - (int)(data.get(i).getWeight());
+            if(j >= 0){
                 try {
                     result.addItem(data.get(i));
                 }catch (ItemCannotBeAddedException e) {
-                    //nop
+                    System.out.println("item " + i + " non ajouté");
                 }
             }
             i--;
         }
-
+        //2 4 5 7 8
         return result;
     }
 }
