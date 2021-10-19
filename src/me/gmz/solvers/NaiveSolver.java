72d368bbea3f93f2867bba820e1579f28897b08c
GuillaumeMZ <guillaume.merchez@etu.u-paris.fr>
2021-10-19 23:46:57 +0200

    Nothing

--- src/me/gmz/solvers/NaiveSolver.java
+++ src/me/gmz/solvers/NaiveSolver.java
@@ -15,7 +15,7 @@ public class NaiveSolver implements ISolver {
         data = new ArrayList<>(data); //We are making a copy of the datalist so the original list order stays the same
 
         QuickSort.sort(data, (first, second) ->
-                (int)Math.signum(second.getRatio() - first.getRatio())
+                (int)Math.signum(second.getRatio() - first.getRatio()) //Sorting in descending order, based on ratio
         );
 
         for (Item current : data) {
